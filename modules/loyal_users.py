03aebe8f75804345be2b1ffe09ffc72e38d7e6b3
Column01 <ColinAndress999@gmail.com>
2019-12-25 13:39:49 -0500

    More bugfixes!
    More bugfixes!

--- modules/loyal_users.py
+++ modules/loyal_users.py
@@ -17,41 +17,45 @@ async def check_loyal_users(client):
         # Loop over every guild the bot is a part of
         for guild in client.guilds:
             guild_id = str(guild.id)
-            # Loop over every member in that guild
-            for member in guild.members:
-                # ignore bots
-                if member.bot:
-                    continue
-                # get their joined time and convert it to datetime format
-                joined = datetime.fromisoformat(str(member.joined_at))
-                # get the current datetime format
-                current = datetime.now()
-                # subtract them to get the days they've been a member
-                days = abs(current-joined).days
-                # Get the server settings file and then get the loyalty roles for the server
-                settings = await jm.get_server_settings()
-                loyalty_roles = settings["guilds"][guild_id]["loyalty_roles"]
-                # Loop over all loyalty roles
-                for role_id in loyalty_roles:
-                    role_days = int(loyalty_roles[role_id])
-                    # If the user has been a member long enough for the role
-                    if days >= role_days:
-                        # Get the index of the next loyalty role in the storage (the old role) and then get the role ID
-                        next_index = list(loyalty_roles.keys()).index(role_id) + 1
-                        old_role_id = list(loyalty_roles.keys())[next_index]
-                        # get the new role and the old role from discord
-                        role = discord.utils.get(guild.roles, id=int(role_id))
-                        old_role = discord.utils.get(guild.roles, id=int(old_role_id))
-                        # if the new role exists on discord
-                        if role is not None:
-                            # and they don't already have it
-                            if not await user_has_role(guild, member.id, int(role_id)):
-                                # Add the new role and remove the old one
-                                if old_role is not None:
-                                    await member.remove_roles(old_role)
-                                await member.add_roles(role, reason="Playtime Requirements")
-                        # break so we don't keep adding roles
-                        break
+            if settings["guilds"].get(guild_id) is not None:
+                # Loop over every member in that guild
+                for member in guild.members:
+                    # ignore bots
+                    if member.bot:
+                        continue
+                    # get their joined time and convert it to datetime format
+                    joined = datetime.fromisoformat(str(member.joined_at))
+                    # get the current datetime format
+                    current = datetime.now()
+                    # subtract them to get the days they've been a member
+                    days = abs(current-joined).days
+                    # Get the server settings file and then get the loyalty roles for the server
+                    settings = await jm.get_server_settings()
+                    loyalty_roles = settings["guilds"][guild_id]["loyalty_roles"]
+                    # Loop over all loyalty roles
+                    for role_id in loyalty_roles:
+                        role_days = int(loyalty_roles[role_id])
+                        # If the user has been a member long enough for the role
+                        if days >= role_days:
+                            # Get the index of the next loyalty role in the storage (the old role) and then get the role ID
+                            next_index = list(loyalty_roles.keys()).index(role_id) + 1
+                            old_role_id = list(loyalty_roles.keys())[next_index]
+                            # get the new role and the old role from discord
+                            role = discord.utils.get(guild.roles, id=int(role_id))
+                            old_role = discord.utils.get(guild.roles, id=int(old_role_id))
+                            # if the new role exists on discord
+                            if role is not None:
+                                # and they don't already have it
+                                if not await user_has_role(guild, member.id, int(role_id)):
+                                    # Add the new role and remove the old one
+                                    if old_role is not None:
+                                        await member.remove_roles(old_role)
+                                    await member.add_roles(role, reason="Playtime Requirements")
+                            # break so we don't keep adding roles
+                            break
+            else:
+                # guild is not in settings file.
+                continue
         # Run every three seconds
         await asyncio.sleep(3)
 
